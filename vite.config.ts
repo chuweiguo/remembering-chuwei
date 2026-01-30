6b6b40fa627361a1f2b9a708b002c6bff53a21a7
chuweiguo <jerrywuzijie+chuwei@gmail.com>
2026-01-30 03:30:06 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/remembering-chuwei/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
